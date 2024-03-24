9bea2ca767190fff6352ab81880a4c9d1f531ddb
ki688774 <ki688774@ucf.edu>
2024-03-24 17:42:26 -0400

    Completed assignment

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -26,38 +26,183 @@ size_t Size(void* ptr)
 	return ((size_t*)ptr)[-1];
 }
 
+void intSwap(int *a, int *b) {
+	int tempInt = *a;
+	*a = *b;
+	*b = tempInt;
+}
+
+void percolateDown(int arr[], int n, int targetIndex) {
+	//Abort the mission if the target index is a leaf or the target index is out of bounds.
+	if ((targetIndex + 1) * 2 > n) {
+		return;
+	}
+
+	//Find the smaller (or only) child of the target value.
+	int swapIndex;
+
+	if ((targetIndex + 1) * 2 < n && arr[(targetIndex + 1) * 2] < arr[((targetIndex + 1) * 2) - 1]) {
+		swapIndex = (targetIndex + 1) * 2;
+	} else {
+		swapIndex = ((targetIndex + 1) * 2) - 1;
+	}
+
+	//Check if the target value is bigger than its smaller (or only) child. If so, percolate.
+	if (arr[targetIndex] > arr[swapIndex]) {
+		intSwap((arr + targetIndex), (arr + swapIndex));
+		percolateDown(arr, n, swapIndex);
+	}
+
+}
+
+//This doesn't automatically reduce n. You'll have to do that yourself.
+int popHeap(int arr[], int n) {
+	if (n < 1) {
+		return 0;
+	}
+
+	intSwap(arr, (arr + n - 1));
+	percolateDown(arr, (n - 1), 0);
+	return arr[n - 1];
+}
+
+//You should only be calling heapify with headIndex as 0. Anything else is for heapify recursion only.
+void heapify(int arr[], int n, int headIndex) {
+	//Abort the mission if the head index is a leaf or the target index is out of bounds.
+	if ((headIndex + 1) * 2 > n) {
+		return;
+	}
+
+	//Call heapify on the children of headIndex (whether or not they exist).
+	heapify(arr, n, ((headIndex + 1) * 2) - 1);
+	heapify(arr, n, (headIndex + 1) * 2);
+
+	//Then, percolate down the head.
+	percolateDown(arr, n, headIndex);
+}
+
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	//First, heapify the array.
+	heapify(arr, n, 0);
+
+	//Then, copy the heap.
+	int *copyArray = Alloc(sizeof(int) * n);
+	for (int c = 0; c < n; c++) {
+		copyArray[c] = arr[c];
+	}
+
+	//Finally, pop the entire copied heap back into the original array.
+	for (int c = 0; c < n; c++) {
+		arr[c] = popHeap(copyArray, (n - c));
+	}
+
+	DeAlloc(copyArray);
 }
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
+	//If the array is 1 or smaller, do nothing.
+	//It should never be smaller, but you never know when some maniac will try to break reality.
+	if (l >= r) {
+		return;
+	}
+
+	//Find the middle of the array.
+	int m = (l + r)/2;
+
+	//Run a merge sort on the each individual half of the array.
+	mergeSort(pData, l, m);
+	mergeSort(pData, (m + 1), r);
+
+	//Then, splice the two halves together.
+	int leftIndex = l;
+	int rightIndex = (m + 1);
+	int *tempArray = Alloc(sizeof(int) * (r + 1 - l));
+	int tempArrayIndex = 0;
+
+	while (leftIndex <= m && rightIndex <= r) {
+		if (pData[leftIndex] < pData[rightIndex]) {
+			tempArray[tempArrayIndex] = pData[leftIndex];
+			leftIndex++;
+		} else {
+			tempArray[tempArrayIndex] = pData[rightIndex];
+			rightIndex++;
+		}
+		tempArrayIndex++;
+	}
+
+	//While statements to dump the rest of the remaining half into tempArray.
+	while (leftIndex <= m) {
+		tempArray[tempArrayIndex] = pData[leftIndex];
+		leftIndex++;
+		tempArrayIndex++;
+	}
+
+	while (rightIndex <= r) {
+		tempArray[tempArrayIndex] = pData[rightIndex];
+		rightIndex++;
+		tempArrayIndex++;
+	}
+
+	//Final part of splicing: put everything from tempArray to its rightful place in pData.
+	for (int c = 0; c < tempArrayIndex; c++) {
+		pData[l + c] = tempArray[c];
+	}
+
+	DeAlloc(tempArray);
 }
 
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
-	
+	for (int activeSlots = 2; activeSlots <= n; (activeSlots)++) {
+		for (int c = activeSlots - 1; c > 0; c--) {
+			if (pData[c] < pData[c - 1]) {
+				intSwap((pData + c), (pData + c - 1));
+			} else {
+				break;
+			}
+		}
+	}
 }
 
 // implement bubble sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
-	
+	for (int unsortedSlots = n; unsortedSlots > 1; unsortedSlots--) {
+		for (int c = 0; c < (unsortedSlots - 1); c++) {
+			if (pData[c] > pData[c + 1]) {
+				intSwap((pData + c), (pData + c + 1));
+			}
+		}
+	}
+
 }
 
 // implement selection sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
-	
+	for (int sortedSlots = 0; sortedSlots < n - 1; sortedSlots++) {
+		int tempIndex = sortedSlots;
+		for (int c = sortedSlots + 1; c < n; c++) {
+			if (pData[c] < pData[tempIndex]) {
+				tempIndex = c;
+			}
+		}
+
+		//Figured that a comparison would probably cost less performance than a swap.
+		if (tempIndex != sortedSlots) {
+			intSwap((pData + tempIndex), (pData + sortedSlots));
+		}
+	}
 }
 
 // parses input file to an integer array
@@ -72,6 +217,11 @@ int parseData(char *inputFileName, int **ppData)
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)Alloc(sizeof(int) * dataSz);
 		// Implement parse data block
+		for (int c = 0; c < dataSz; c++) {
+			fscanf(inFile, "%d ", *ppData + c);
+		}
+
+		fclose(inFile);
 	}
 	
 	return dataSz;
@@ -160,11 +310,14 @@ int main(void)
 		printf("\textra memory allocated\t: %d\n",extraMemoryAllocated);
 		printArray(pDataCopy, dataSz);
 
-                printf("Heap Sort:\n");
+        printf("Heap Sort:\n");
 		memcpy(pDataCopy, pDataSrc, dataSz*sizeof(int));
 		extraMemoryAllocated = 0;
 		start = clock();
-		heapSort(pDataCopy, 0, dataSz - 1);
+		//Despite heapSort only having two arguments, the original line of code feeds in three.
+		//I replaced the line with what it (probably) should be, but the original is still here (just commented out).
+		//heapSort(pDataCopy, 0, dataSz - 1);
+		heapSort(pDataCopy, dataSz);
 		end = clock();
 		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 		printf("\truntime\t\t\t: %.1lf\n",cpu_time_used);
